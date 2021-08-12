db7d8a73b83b573923efac73e37eaaa039a0e052
2021096862 <hanyang.ac.kr>
2021-08-12 15:29:33 +0900

    activity_main.xml, recy_item.xml 구현.

---
+++ app/src/main/java/com/example/phonebook_esc/Contact.java
@@ -0,0 +1,21 @@
+package com.example.phonebook_esc;
+
+public class Contact {
+    private String name;
+    private String phNum;
+
+    public Contact() {
+
+    }
+
+    public Contact(String name, String phNum) {
+
+    }
+
+    public String getPhNum() { return phNum; }
+    public void setPhNum(String phNum){this.phNum = phNum;}
+
+    public String getName() { return name; }
+    public void setName(String name){this.name = name;}
+
+}
